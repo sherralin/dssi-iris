46fc2b9c93bd9c37f39d1986e34c73aba67857d7
ChristabelChou <christabelchou@gmail.com>
2025-05-31 17:07:00 +0800

    2nd commit

--- app.py
+++ app.py
@@ -10,11 +10,11 @@ setosa, versicolor, virginica')
 st.header("Plant Features")
 col1, col2 = st.columns(2)
 with col1:
-st.text("Sepal characteristics")
+    st.text("Sepal characteristics")
 sepal_l = st.slider('Sepal lenght (cm)', 1.0, 8.0, 0.5)
 sepal_w = st.slider('Sepal width (cm)', 2.0, 4.4, 0.5)
 with col2:
-st.text("Pepal characteristics")
+    st.text("Pepal characteristics")
 petal_l = st.slider('Petal lenght (cm)', 1.0, 7.0, 0.5)
 petal_w = st.slider('Petal width (cm)', 0.1, 2.5, 0.5)
 
