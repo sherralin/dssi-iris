ae00b8208559ab719b2fefeadb5ca3ae7913edf9
chuavincent89 <vincentchuahongpu@gmail.com>
2025-05-31 17:16:59 +0800

    prediction v2

--- prediction.py
+++ prediction.py
@@ -5,8 +5,3 @@ import numpy as np
 def predict(data):
     clf = joblib.load("rf_model.sav")
     return clf.predict(data)
-
-from prediction import predict
-if st.button("Predict type of Iris"):
-             result = predict(np.array([[sepal_l, sepal_w, petal_l, petal_w]]))
-st.text(result[0])
