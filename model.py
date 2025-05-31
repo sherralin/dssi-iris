d3c498e38c662a5431b8195b54850c837301c581
chuavincent89 <vincentchuahongpu@gmail.com>
2025-05-31 16:26:02 +0800

    model.py version 1.0

--- model.py
+++ model.py
@@ -1,9 +1,23 @@
+import pandas as pd
+import numpy as np
+import joblib
+import os
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+from sklearn.pipeline import Pipeline
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import classification_report, f1_score, accuracy_score
+
+seed = 49
+
 # Read original dataset
-iris_df = pd.read_csv(“data/iris.csv”)
+iris_df = pd.read_csv('data/iris.csv')
 iris_df.sample(frac=1, random_state=seed)
 # selecting features and target data
-X = iris_df[[‘SepalLengthCm’, ‘SepalWidthCm’, ‘PetalLengthCm’, ‘PetalWidthCm’]]
-y = iris_df[[‘Species’]]
+X = iris_df[['SepalLengthCm', 'SepalWidthCm', 'PetalLengthCm', 'PetalWidthCm']]
+y = iris_df[['Species']]
+
 # split data into train and test sets
 # 70% training and 30% test
 X_train, X_test, y_train, y_test = train_test_split(
@@ -16,4 +30,8 @@ clf.fit(X_train, y_train)
 y_pred = clf.predict(X_test)
 # calculate accuracy
 accuracy = accuracy_score(y_test, y_pred)
-print(f”Accuracy: {accuracy}”) # Accuracy: 0.91
+print(f"Accuracy: {accuracy}") # Accuracy: 0.91
+      
+
+# save the model to disk
+joblib.dump(clf, "rf_model.sav")
